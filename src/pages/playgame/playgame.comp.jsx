01e44f2525545db87737d837943d2735818d3b49
Laszlo Tarkanyi <tarkanyil@gmail.com>
2021-04-05 00:53:42 +0200

    Feat: added scrollToTop and fade-in&fade-out animation to round submission during gameplay

--- src/pages/playgame/playgame.comp.jsx
+++ src/pages/playgame/playgame.comp.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import { useSelector, useDispatch } from "react-redux";
-import { withRouter, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import { withRouter, useHistory } from 'react-router-dom';
 
-import "./playgame.styles.css";
-import image from "../../assets/leader-badge.png";
+import './playgame.styles.css';
+import image from '../../assets/leader-badge.png';
 
 import {
   resetState,
   roundSubmit,
-  gameRollback,
-} from "../../redux/game/game.actions";
+  gameRollback
+} from '../../redux/game/game.actions';
 import {
   updatePlayerRoundPoints,
   updatePlayerPhase,
   endOfRoundUpdate,
   newRoundSamePlayers,
-  playerRollback,
-} from "../../redux/player/player.actions";
+  playerRollback
+} from '../../redux/player/player.actions';
 
-import { numberOfPhases, phases } from "../../utils/constants";
+import { numberOfPhases, phases } from '../../utils/constants';
 
-import Header from "../../components/header/header.comp";
+import Header from '../../components/header/header.comp';
 
 const PlayGame = () => {
   const players = useSelector((state) => state.players);
@@ -30,12 +30,14 @@ const PlayGame = () => {
 
   const [isInputError, setIsInputError] = useState({
     isError: false,
-    playerId: "",
+    playerId: ''
   });
 
-  const [inputErrorMessage, setInputErrorMessage] = useState("");
+  const [inputErrorMessage, setInputErrorMessage] = useState('');
 
-  const [rollbackButtonEnabled, setRollbackButtonEnabled] = useState(true)
+  const [rollbackButtonEnabled, setRollbackButtonEnabled] = useState(true);
+
+  const [submitDarkness, setSubmitDarkness] = useState(false);
 
   let gameCompleted = false;
 
@@ -44,12 +46,12 @@ const PlayGame = () => {
   const handlePointsInput = (event) => {
     const value = event.target.value;
     const idx = event.target.name;
-    isInputError.isError && setIsInputError({ isError: false, playerId: "" });
+    isInputError.isError && setIsInputError({ isError: false, playerId: '' });
     if (isNaN(value)) {
       setIsInputError({ isError: true, playerId: idx });
-      setInputErrorMessage("Please enter a numeric value");
+      setInputErrorMessage('Please enter a numeric value');
       //update playerPoint to nixxx
-      dispatch(updatePlayerRoundPoints({ idx: idx, value: "" }));
+      dispatch(updatePlayerRoundPoints({ idx: idx, value: '' }));
       return;
     } else {
       dispatch(updatePlayerRoundPoints({ idx: idx, value: value }));
@@ -64,6 +66,8 @@ const PlayGame = () => {
     setTimeout(() => updateGameState(), 1);
 
     setRollbackButtonEnabled(true);
+
+    handleSubmitAnimation();
   };
 
   const updatePlayerState = () => {
@@ -72,13 +76,13 @@ const PlayGame = () => {
 
   const updateGameState = () => {
     gameCompleted = checkGameCompleted();
-    console.log("updateGameState checkGameComp", gameCompleted);
+    console.log('updateGameState checkGameComp', gameCompleted);
     const nextGiver = givesCardsNext(game.givesCard);
     dispatch(
       roundSubmit({
         nextGiver: nextGiver,
         gameCompleted: gameCompleted,
-        prevGiver: game.givesCard,
+        prevGiver: game.givesCard
       })
     );
   };
@@ -123,7 +127,7 @@ const PlayGame = () => {
     let idxWinner = 0;
     let minPointAmount = 1000;
     const playersCompleted = players.filter(
-      (player) => player.actualPhase == "GAME COMPLETED"
+      (player) => player.actualPhase == 'GAME COMPLETED'
     );
     if (playersCompleted.length > 1) {
       playersCompleted.map((player, idx) => {
@@ -137,7 +141,7 @@ const PlayGame = () => {
     return playersCompleted[idxWinner].name;
   };
 
-  const handleResetWithNewPlayers = () => history.push("/startgame");
+  const handleResetWithNewPlayers = () => history.push('/startgame');
 
   const isWinner = (name) => {
     if (game.completed && name == determineWinner()) {
@@ -159,84 +163,104 @@ const PlayGame = () => {
     setRollbackButtonEnabled(false);
   };
 
+  const handleSubmitAnimation = () => {
+    const modal = document.getElementById('modal');
+    const modalText = document.getElementById('modal-text');
+    modal.classList.add('my-modal');
+    modalText.classList.remove('modal-text-hidden');
+    modalText.classList.add('modal-text');
+    setTimeout(() => window.scrollTo({ top: 0, behavior: 'auto' }), 800);
+    setTimeout(() => {
+      modal.classList.remove('my-modal');
+      modalText.classList.remove('modal-text');
+      modalText.classList.add('modal-text-hidden');
+    }, 3000);
+  };
+
   return (
     <div>
+      <div id='modal' className=''>
+        <h1 id='modal-text' className='modal-text-hidden'>
+          Next round!
+        </h1>
+      </div>
       <Header />
-      <div className="container">
-        <h2 className="header-title mt-3">Let's play!</h2>
+      <div className='container'>
+        <h2 className='header-title mt-3'>Let's play!</h2>
         {!game.completed ? (
           <div>
-            <h2 className="mt-4 mb-4">Actual round: {game.count}</h2>
-            <h2 className="mt-4 mb-4">Mixes cards: {game.givesCard}</h2>
+            <h2 className='mt-4 mb-4'>Actual round: {game.count}</h2>
+            <h2 className='mt-4 mb-4'>Mixes cards: {game.givesCard}</h2>
           </div>
         ) : (
           <div>
-            <h2 className="mt-4 mb-4">GAME COMPLETED</h2>
-            <h2 className="mt-4 mb-4">The winner is: {determineWinner()}</h2>
+            <h2 className='mt-4 mb-4'>GAME COMPLETED</h2>
+            <h2 className='mt-4 mb-4'>The winner is: {determineWinner()}</h2>
           </div>
         )}
-        <form onSubmit={handleSubmit} className="playgame-form">
-          <div className="row ">
+        <form onSubmit={handleSubmit} className='playgame-form'>
+          <div className='row '>
             {players.map((player, idx) => {
               return (
-                <div className="col-sm-12 col-md-6 col-lg-4" key={idx}>
-                  <div className="card text-dark bg-light mb-3">
-                    <div className="card-header">{player.name}</div>
+                <div className='col-sm-12 col-md-6 col-lg-4' key={idx}>
+                  <div className='card text-dark bg-light mb-3'>
+                    <div className='card-header'>{player.name}</div>
                     {isWinner(player.name) ? (
                       <img
                         src={image}
-                        alt="leader badge"
-                        className="leader-image"
+                        alt='leader badge'
+                        className='leader-image'
                       />
                     ) : null}
 
-                    <div className="card-body">
-                      <h6 className="card-title">
+                    <div className='card-body'>
+                      <h6 className='card-title'>
                         Actual phase: {player.actualPhase}
                       </h6>
-                      <h6 className="card-subtitle">
-                        {(player.actualPhase !== "GAME COMPLETED") ? (phases[player.actualPhase - 1].desc) : "All phases checked!"}
+                      <h6 className='card-subtitle'>
+                        {player.actualPhase !== 'GAME COMPLETED'
+                          ? phases[player.actualPhase - 1].desc
+                          : 'All phases checked!'}
                       </h6>
                       <hr />
-                      <h6 className="card-title">Round result</h6>
-                      <div className="mb-3 form-check">
+                      <h6 className='card-title'>Round result</h6>
+                      <div className='mb-3 form-check'>
                         <input
                           name={idx}
-                          type="checkbox"
-                          className="form-check-input"
-                          id="exampleCheck1"
+                          type='checkbox'
+                          className='form-check-input'
+                          id='exampleCheck1'
                           value={player.phaseCompleted}
                           onChange={handleCheckBox}
                           checked={player.phaseCompleted}
                         />
                         <label
-                          className="form-check-label"
-                          htmlFor="exampleCheck1"
-                        >
+                          className='form-check-label'
+                          htmlFor='exampleCheck1'>
                           Phase completed
                         </label>
                       </div>
 
-                      <div className="input-group mb-3">
-                        <span className="input-group-text" id="basic-addon3">
+                      <div className='input-group mb-3'>
+                        <span className='input-group-text' id='basic-addon3'>
                           Points
                         </span>
                         <input
                           key={idx}
                           name={idx}
-                          type="text"
-                          className="form-control"
-                          id="basic-url"
-                          aria-describedby="basic-addon3"
+                          type='text'
+                          className='form-control'
+                          id='basic-url'
+                          aria-describedby='basic-addon3'
                           value={players[idx].roundPoints}
                           onChange={handlePointsInput}
                         />
                       </div>
                       {isErrorWith(idx) ? (
-                        <p className="error-msg">*{inputErrorMessage}</p>
+                        <p className='error-msg'>*{inputErrorMessage}</p>
                       ) : null}
                       <hr />
-                      <h6 className="card-title">
+                      <h6 className='card-title'>
                         Total points: {player.totalPoints}
                       </h6>
                     </div>
@@ -246,28 +270,25 @@ const PlayGame = () => {
             })}
           </div>
           {!game.completed && (
-            <button className="btn btn-lg btn-warning mt-3 mb-4">
+            <button className='btn btn-lg btn-warning mt-3 mb-4'>
               Submit round
             </button>
           )}
         </form>
         <button
-          className="btn btn-lg btn-outline-warning mt-1 mb-4"
+          className='btn btn-lg btn-outline-warning mt-1 mb-4'
           onClick={handleRollback}
-          disabled={rollbackButtonEnabled ? false : true}
-        >
+          disabled={rollbackButtonEnabled ? false : true}>
           Undo last round submission
         </button>
         <button
-          className="btn btn-lg btn-outline-primary mb-4 new-game"
-          onClick={handleNewSame}
-        >
+          className='btn btn-lg btn-outline-primary mb-4 new-game'
+          onClick={handleNewSame}>
           New game with the same players
         </button>
         <button
-          className="btn btn-lg btn-outline-success mb-4 new-game"
-          onClick={handleResetWithNewPlayers}
-        >
+          className='btn btn-lg btn-outline-success mb-4 new-game'
+          onClick={handleResetWithNewPlayers}>
           Restart with new players
         </button>
       </div>
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,4 @@
-export const numberOfPhases = 10;
+export const numberOfPhases = 2;
 
 export const phases = [
     {num: 1, desc: "2 sets of 3"},
@@ -6,7 +6,7 @@ export const phases = [
     {num: 3, desc: "1 set of 4 + 1 run of 4"},
     {num: 4, desc: "1 run of 7"},
     {num: 5, desc: "1 run of 8"},
-    {num: 6, desc: "1 run of 4"},
+    {num: 6, desc: "1 run of 9"},
     {num: 7, desc: "2 sets of 4"},
     {num: 8, desc: "7 cards of one color"},
     {num: 9, desc: "1 set of 5 + 1 set of 2"},
